7961fbbea24c53553b861802b4df72af91b4e72e
StarkMaliyk <starkmaliyk@gmail.com>
2022-12-11 19:00:31 +0530

    AI Commit

--- food.py
+++ food.py
@@ -1,7 +1,9 @@
 from turtle import Turtle
 import random
 
+
 class Food(Turtle):
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -12,6 +14,6 @@ class Food(Turtle):
         self.refresh()
 
     def refresh(self):
-        random_x = random.randint(-250, 250)
-        random_y = random.randint(-250, 250)
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
--- main.py
+++ main.py
@@ -1,48 +1,52 @@
 from turtle import Screen
-from food import Food
 from snake import Snake
-import time
+from food import Food
 from scoreboard import Scoreboard
+import time
 
-scr = Screen()
-scr.setup(width=600, height=600)
-scr.bgcolor("black")
-scr.tracer(0)
-scr.title("My Snake Game")
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
-scr.listen()
-scr.onkey(snake.up, "Up")
-scr.onkey(snake.down, "Down")
-scr.onkey(snake.right, "Right")
-scr.onkey(snake.left, "Left")
-segments = []
-
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
-    scr.update()
+    screen.update()
     time.sleep(0.1)
     snake.move()
 
-    # Detect Collision
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
-    
-    # Detect Collison with wall
+
+    #Detect collision with wall.
     if snake.head.xcor() > 290 or snake.head.xcor() < -300 or snake.head.ycor() > 300 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
-    # Detect collsion with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
 
-scr.exitonclick()
+
+
+
+
+screen.exitonclick()
